87d4d190fca7a7b2cf893c35801d21041a9eb3e0
rwein03 <rwein03@gmail.com>
2016-12-01 20:15:49 +0700

    fuaaack
    aosdkfoaskdfoadsf

--- Uas.py
+++ Uas.py
@@ -1,53 +1,81 @@
+from __future__ import print_function
 import httplib2
 import os
-import logging
 
+from apiclient import discovery
+from oauth2client import client
 from oauth2client import tools
-# from oauth2client import run
 from oauth2client.file import Storage
-# from oauth2client.client import AccessTokenRefreshError
-from googleapiclient.discovery import build
-from oauth2client.client import flow_from_clientsecrets
-from googleapiclient.errors import HttpError
-import json
 
-CLIENT_SECRETS_FILE = "client_secret.json"
-YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-MISSING_CLIENT_SECRETS_MESSAGE = "Missing client secrets file"
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
 
 
-def authenticate():
-    httplib2.debuglevel = 4
-    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-                                   scope=YOUTUBE_READ_WRITE_SCOPE,
-                                   message=MISSING_CLIENT_SECRETS_MESSAGE)
-    storage = Storage("%s-oauth2.json")
-    credentials = storage.get()
-    if credentials is None or credentials.invalid:
-        print('invalid credentials')
-        credentials = run_flow(flow, storage)
+def get_credentials():
+    """Gets valid user credentials from storage.
 
-    service = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-                    http=credentials.authorize(httplib2.Http()))
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
 
-    tags = "classical music", "yehudi mehunin"
-    body = dict(
-        snippet=dict(
-            title="some title",
-            description="a  description",
-            tags=tags,
-            categoryId="4"
-        ),
-        status=dict(
-            privacyStatus="Private"
-        )
-    )
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
 
-    thingy = service.videos().insert(part=",".join(body.keys()), body=None, media_body=MediaFileUpload(
-        "1977.mp4", mimetype="video/mp4", chunksize=1024 * 1024, resumable=False))
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
 
-    thingy.execute()
+def main():
+    """Shows basic usage of the Sheets API.
 
-authenticate()
+    Creates a Sheets API service object and prints the names and majors of
+    students in a sample spreadsheet:
+    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+    rangeName = 'Class Data!A2:E'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+
+
+if __name__ == '__main__':
+    main()
