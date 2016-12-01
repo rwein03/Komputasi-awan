a07c8307304c7af137b3de94df44cacff5cb9d90
rwein03 <rwein03@gmail.com>
2016-12-01 19:11:15 +0700

    asdfdasf
    asdfasdf

--- Uas.py
+++ Uas.py
@@ -1,74 +1,49 @@
-#jancok
-from __future__ import print_function
+import base64
 import httplib2
-import os
 
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
+from email.mime.text import MIMEText
+
+from apiclient.discovery import build
+from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
+from oauth2client.tools import run
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
 
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/gmail-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+# Path to the client_secret.json file downloaded from the Developer Console
 CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Gmail API Python Quickstart'
 
+# Check https://developers.google.com/gmail/api/auth/scopes for all available scopes
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/gmail.compose'
 
-def get_credentials():
-    """Gets valid user credentials from storage.
+# Location of the credentials storage file
+STORAGE = Storage('gmail.storage')
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+# Start the OAuth flow to retrieve credentials
+flow = flow_from_clientsecrets(CLIENT_SECRET_FILE, scope=OAUTH_SCOPE)
+http = httplib2.Http()
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'gmail-python-quickstart.json')
+# Try to retrieve credentials from storage or run the flow to generate them
+credentials = STORAGE.get()
+if credentials is None or credentials.invalid:
+  credentials = run(flow, STORAGE, http=http)
 
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+# Authorize the httplib2.Http object with our credentials
+http = credentials.authorize(http)
 
-def main():
-    """Shows basic usage of the Gmail API.
+# Build the Gmail service from discovery
+gmail_service = build('gmail', 'v1', http=http)
 
-    Creates a Gmail API service object and outputs a list of label names
-    of the user's Gmail account.
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
+# create a message to send
+message = MIMEText("Message goes here.")
+message['to'] = "yourvictim@goes.here"
+message['from'] = "you@go.here"
+message['subject'] = "your subject goes here"
+body = {'raw': base64.b64encode(message.as_string())}
 
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
-
-    if not labels:
-        print('No labels found.')
-    else:
-      print('Labels:')
-      for label in labels:
-        print(label['name'])
-
-
-if __name__ == '__main__':
-    main()
+# send it
+try:
+  message = (gmail_service.users().messages().send(userId="me", body=body).execute())
+  print('Message Id: %s' % message['id'])
+  print(message)
+except Exception as error:
+  print('An error occurred: %s' % error)
