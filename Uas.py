5537c8d5e1f85deeba7cc34383c31092b35747ad
rwein03 <rwein03@gmail.com>
2016-12-01 18:21:11 +0700

    123123213

--- Uas.py
+++ Uas.py
@@ -1,63 +1,73 @@
+from __future__ import print_function
 import httplib2
 import os
-import oauth2client
-from oauth2client import client, tools
-import base64
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from apiclient import errors, discovery
 
-SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/gmail-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Gmail API Python Send Email'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+
 
 def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'gmail-python-email-send.json')
-    store = oauth2client.file.Storage(credential_path)
+                                   'gmail-python-quickstart.json')
+
+    store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        credentials = tools.run_flow(flow, store)
-        print 'Storing credentials to ' + credential_path
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
     return credentials
 
-def SendMessage(sender, to, subject, msgHtml, msgPlain):
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
-    message1 = CreateMessage(sender, to, subject, msgHtml, msgPlain)
-    SendMessageInternal(service, "me", message1)
 
-def SendMessageInternal(service, user_id, message):
-    try:
-        message = (service.users().messages().send(userId=user_id, body=message).execute())
-        print 'Message Id: %s' % message['id']
-        return message
-    except errors.HttpError, error:
-        print 'An error occurred: %s' % error
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
 
-def CreateMessage(sender, to, subject, msgHtml, msgPlain):
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = to
-    msg.attach(MIMEText(msgPlain, 'plain'))
-    msg.attach(MIMEText(msgHtml, 'html'))
-    return {'raw': base64.urlsafe_b64encode(msg.as_string())}
+    if not labels:
+        print('No labels found.')
+    else:
+      print('Labels:')
+      for label in labels:
+        print(label['name'])
 
-def main():
-    to = "to@address.com"
-    sender = "from@address.com"
-    subject = "subject"
-    msgHtml = "Hi<br/>Html Email"
-    msgPlain = "Hi\nPlain Email"
-    SendMessage(sender, to, subject, msgHtml, msgPlain)
 
 if __name__ == '__main__':
     main()
